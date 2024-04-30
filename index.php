d8069377a2583b77a4db078f694eb63d11490d41
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-04-30 17:48:22 +0000

    Main

--- index.php
+++ index.php
@@ -1,9 +1,77 @@
-
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <p>Paragraph</p>
-  </body>
+
+<head>
+  <!-- Meta data section -->
+  <meta charset="utf-8">
+  <meta name="description" content="Widget to figure out how much a person has to pay for admission in the dinosaur Museum of Ottawa, Using PHP">
+  <meta name="keywords" content="immaculata, icd2o">
+  <meta name="author" content="Atri Sarker">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/favicon-16x16.png">
+  <link rel="manifest" href="./fav_index/site.webmanifest">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="./css/style.css">
+
+  <!-- MDL -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+  <!-- Title -->
+  <title>Museum Admission in PHP</title>
+</head>
+
+<body>
+  
+  <!-- Header -->
+  <h1>Museum Admission in PHP</h1>
+
+  <!-- INPUT FORM [used for form validation] -->
+  <form id="inputForm" method="post" action="results.php" target="resultFrame">
+    <!-- Table For Layout -->
+    <table>
+      <tr>
+        <td>
+          <!-- age Input -->
+          <label for="ageInput">Enter age:</label>
+          <!-- Step, Min, and Max are undefined for sake of assignment -->
+          <input id="ageInput" type="number" name="age" value="" step="any" min="" max="" />
+          <br>
+          <!-- Day of the week Input -->
+          <label for="dayInput">Select day of the week:</label>
+          <select id="dayInput" name="day">
+            <option value="Monday">Monday</option>
+            <option value="Tuesday">Tuesday</option>
+            <option value="Wednesday">Wednesday</option>
+            <option value="Thursday">Thursday</option>
+            <option value="Friday">Friday</option>
+            <option value="Saturday">Saturday</option>
+            <option value="Sunday">Sunday</option>
+          </select>
+        </td>
+        <td>
+          <!-- Image of PHP LOGO -->
+          <img class="contentImg" src="./images/phpLogo.svg" alt="PHP Logo!">
+          <br>
+        </td>
+      </tr>
+    </table>
+
+    <!-- Results Button -->
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit"
+      value="submit" id="resultBtn">Get Result!</button>
+  </form>
+
+  <!-- Result -->
+  <iframe name="resultFrame" src="./results.php">
+  </iframe>
+
+</body>
+
 </html>
--- results.php
+++ results.php
@@ -1,16 +1,50 @@
-
-<!-- Result -->
-  <iframe name="resultFrame" src="./results.php">
-  </iframe>
-<!-- <form id="inputForm" method="post" action="results.php" target="resultFrame"> -->
-
 <b>
   <?php
     // Code
-    if(isset($_POST['submit'])) 
+    if(isset($_POST["age"])) 
     {
-      // INPUT
-      $input = floatval($_POST['{value}']);
+      // Get user input, years of age
+      $age = floatval($_POST["age"]);
+
+      // Get day of week
+      $day = $_POST["day"];
+
+      // Scenarios:
+
+      // When the user does not enter their age, it should prompt the user to enter their age
+      // When the user does not select a day, it should prompt the user to enter a day
+      // When the user’s age is negative, it should prompt the user to enter a valid age
+      // When the user’s age is a decimal, it should prompt the user to enter a valid age
+
+      // if they are younger than 5 or older than 95, they get in for FREE
+      // otherwise, if they are between 12 and 21 years old, OR if it is Tuesday or Thursday, they get a DISCOUNT (student price)
+      // otherwise they have to pay REGULAR price
+
+      $message = "";
+      if (is_nan($age)) {
+        // Redundant, because floatval() automatically parses empty input as 0
+        $message = "Please Enter Positive Integer for Age!";
+      }
+      else if (($age <= 0) || ($age % 1 != 0)) {
+        $message = "Please Enter Positive Integer for Age";
+      }
+      else if (!$day) {
+        // When the user does not select a day, it should prompt the user to enter a day
+        // Redundant, this situation cannot happen
+        $message = "Please Enter a Day";
+      }
+      else if ($age < 5 || $age > 95) {
+        $message = "You can get in for FREE! 🥳🎉";
+      }
+      else if (($age > 12 && $age < 21) || ($day=="Tuesday" || $day=="Thursday")) {
+        $message = "You get a discount! 🥳";
+      }
+      else {
+        $message = "YOU PAY REGULAR PRICE! 👹👺👿 ";
+      }
+
+      // Display Result
+      echo $message;
     }
     else {
       // Default
